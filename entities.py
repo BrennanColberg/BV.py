1a20b65dea3ad9addda06fe63bc3f6b8be8b64fa
Brennan Colberg <brennan.colberg@gmail.com>
2018-10-16 14:43:30 -0700

    Standardized polygon-based rendering for Entities

--- entities.py
+++ entities.py
@@ -1,7 +1,7 @@
 import pygame
 import math
 import random
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, box
 # other project sources
 import renderer
 from colors import * 
@@ -11,7 +11,11 @@ class Entity:
 	
 	# things connected to entity that die with it (projectiles, etc)
 	children = set()
+	# display
 	shape = None
+	size = 10
+	color = RED
+	stroke = 0
 	# position
 	pos = [0, 0] # x, y
 	heading = 0
@@ -30,19 +34,24 @@ class Entity:
 		self.pos[1] += self.speed * math.sin(self.heading)
 		
 	def render(self):
-		pass
+		if self.shape != None:
+			renderer.draw(self.pos, self.heading, 
+						  self.shape, self.size, self.color, self.stroke)
 	
 class Blob(Entity):
 	
-	size = None
-	
 	def __init__(self, pos, size):
-		Entity.__init__(self, pos, 0, 0)
-		self.size = size
-		
-	def render(self):
-		pygame.draw.circle(renderer.screen, GREEN, renderer.screenCoords(self.pos), 10)
+		Entity.__init__(self, pos, 0, 0);
+		# rendering
+		self.size = 15
+		self.shape = Polygon([
+			(1, 0),
+			(0, -1),
+			(-1, 0),
+			(0, 1)
+		])
 
+		
 class Ship(Entity):
 		
 	def fire(self, speed):
@@ -51,40 +60,41 @@ class Ship(Entity):
 		
 class Player(Ship):
 	
+	def __init__(self, pos, heading, speed):
+		Entity.__init__(self, pos, heading, speed)
+		# rendering
+		self.size = 15
+		self.color = BLUE
+		self.stroke = 5
+		self.shape = Polygon([
+			(1, 0),
+			(-1, 1),
+			(-1, -1)
+		])
+	
 	def keyInput(self, event, pressed):
 		if pressed:
 			if event.key == pygame.K_SPACE:
 				self.fire(4)
 				
 	def tick(self):
-		
 		# points velocity towards mouse position
 		mousepos = renderer.worldCoords(pygame.mouse.get_pos())
 		dx = self.pos[0] - mousepos[0]
 		dy = self.pos[1] - mousepos[1]
 		self.heading = math.atan2(dy, dx) + math.pi
-		
 		# parent calculations (physics etc)
 		Entity.tick(self)
-	
-	def render(self):
-		pos = renderer.screenCoords(self.pos)
-		rect = pygame.Rect(pos, [20, 20])
-		pygame.draw.rect(renderer.screen, RED, rect)
-		renderer.draw(self.pos, self.heading, Polygon([
-			(1, 0),
-			(-1, 1),
-			(-1, -1)
-		]), 50, GREEN, 4)
 		
 
 class Projectile(Entity):
 	
 	def __init__(self, parent, speed):
-		pos = parent.pos.copy()
-		speed += parent.speed
-		heading = parent.heading
-		Entity.__init__(self, pos, heading, speed)
-		
-	def render(self):
-		pygame.draw.circle(renderer.screen, BLUE, renderer.screenCoords(self.pos), 5, 1)
+		# kinematics
+		self.pos = parent.pos.copy()
+		self.heading = parent.heading
+		self.speed += parent.speed + speed
+		# rendering
+		self.size = 3
+		self.color = BLUE
+		self.shape = box(-1, -0.1, 1, 0.1)
