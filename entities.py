c5c790d7ade3a6fbf639dd7fce83bc2b9568c4fd
Brennan Colberg <brennan.colberg@gmail.com>
2018-10-16 07:49:33 -0700

    "Player" class & keyboard input

--- entities.py
+++ entities.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 import random
 from colors import * 
 
@@ -7,48 +8,55 @@ class Entity:
 	# things connected to entity that die with it (projectiles, etc)
 	children = set()
 	# position
-	x = 0
-	y = 0
+	pos = [0, 0] # x, y
+	vel = [0, 0] # heading, speed
 	
-	def __init__(self, x, y):
-		self.x = x;
-		self.y = y;
+	def __init__(self, pos, vel):
+		self.pos = pos;
+		self.vel = vel;
 		
-	def tick(self):
+	def keyInput(self, event, pressed):
 		pass
+	
+	def tick(self):
+		heading = self.vel[0]
+		speed = self.vel[1]
+		self.pos[0] += speed * math.cos(heading)
+		self.pos[1] += speed * math.sin(heading)
+		
 	def render(self, screen):
 		pass
+	
 
 class Ship(Entity):
-	
-	def __init__(self, x, y):
-		Entity.__init__(self, x, y)
-	
-	def tick(self):
-		self.x += 1
-		self.y += 2
-		if random.random() < 0.1:
-			self.fire(random.uniform(1, 5), random.uniform(-0.1, 0.1))
 		
+	def fire(self, speed):
+		self.children.add(Projectile(self, speed))
+		
+		
+class Player(Ship):
+	
+	def keyInput(self, event, pressed):
+		if pressed:
+			if event.key == pygame.K_SPACE:
+				self.fire(4)
+	
 	def render(self, screen):
-		pygame.draw.rect(screen, GREEN, [int(self.x), int(self.y), 40, 40], 0)
+		x = int(self.pos[0])
+		y = int(self.pos[1])
+		rect = pygame.Rect(x, y, 20, 20)
+		pygame.draw.rect(screen, RED, rect)
 		
-	def fire(self, vx, vy):
-		self.children.add(Projectile(self, vx, vy))
 
 class Projectile(Entity):
 	
-	vx = 0
-	vy = 0
-	
-	def __init__(self, parent, vx, vy):
-		Entity.__init__(self, parent.x, parent.y)
-		self.vx = vx
-		self.vy = vy
-		
-	def tick(self):
-		self.x += self.vx
-		self.y += self.vy
+	def __init__(self, parent, speed):
+		pos = parent.pos.copy()
+		vel = parent.vel.copy()
+		vel[1] = speed
+		Entity.__init__(self, pos, vel)
 		
 	def render(self, screen):
-		pygame.draw.circle(screen, RED, [int(self.x), int(self.y)], 5, 1)
+		x = int(self.pos[0])
+		y = int(self.pos[1])
+		pygame.draw.circle(screen, RED, (x, y), 5, 1)
--- game.py
+++ game.py
@@ -7,7 +7,7 @@ from colors import *
 
 # game variables
 objects = set()
-objects.add(Ship(10, 10))
+objects.add(Player([0, 0], [1, 0.5]))
 
 # starting game
 done = False
@@ -19,25 +19,35 @@ clock = pygame.time.Clock()
 
 # game loop
 while not done:
+
 	# TPS/FPS rate
 	clock.tick(60)
-	# quitting test loop
+	
+	# event loop
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
+		elif event.type == pygame.KEYDOWN:
+			for item in objects:
+				item.keyInput(event, True)
+		elif event.type == pygame.KEYUP:
+			for item in objects:
+				item.keyInput(event, False)
+
 	# game logic
 	for item in objects:
 		for child in item.children:
 			child.tick()
 		item.tick()
-	# clearing screen
+	
+	# clear screen
 	screen.fill(WHITE)
-	# drawing code!
+	# render objects
 	for item in objects:
 		for child in item.children:
 			child.render(screen)
 		item.render(screen)
-	# displaying drawing
+	# display new screen
 	pygame.display.flip()
 
 # stop game
